d5f7a0a4931e66287686c30aa6227b646d6759c4
nijo-a <nijo@atki.dk>
2026-08-24 15:02:16 +0200

    Add actions

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
 use anyhow::{Context, Result, anyhow};
-use clap::Parser;
+use clap::{Parser, ValueEnum};
+use serde::Serialize;
 use sqlglot_rust::{Dialect, parse};
 use std::{fs, path::PathBuf};
+
+#[derive(Clone, Copy, Debug, Serialize, PartialEq, ValueEnum)]
+#[serde(rename_all = "kebab-case")]
+enum VmgAction {
+    Add,
+    Check,
+}
 
 #[derive(Parser, Debug)]
 #[command(version)]
 struct Args {
+    action: VmgAction,
+
     #[arg(help = "Migration directory")]
     directory: PathBuf,
 }
 
-fn main() -> Result<()> {
-    let args = Args::parse();
-
-    if !args.directory.is_dir() {
-        return Err(anyhow!("Migration directory is a file"));
-    }
-
-    let files = fs::read_dir(&args.directory)
-        .with_context(|| format!("Failed to read directory '{:?}'", &args.directory))?;
+fn validate_files(directory: &PathBuf) -> Result<()> {
+    let files = fs::read_dir(directory)
+        .with_context(|| format!("Failed to read directory '{:?}'", directory))?;
 
     for file in files {
         let entry = file?;
@@ -40,6 +44,21 @@ fn main() -> Result<()> {
             println!("{file_name} failed");
         };
     }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    if !args.directory.is_dir() {
+        return Err(anyhow!("Migration directory is a file"));
+    }
+
+    match args.action {
+        VmgAction::Add => {}
+        VmgAction::Check => validate_files(&args.directory)?,
+    };
 
     Ok(())
 }
