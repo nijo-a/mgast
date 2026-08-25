acdda7c2a1a5bf5a9d5a3d47186befeba7a9a6f0
nijo-a <nijo@atki.dk>
2026-08-25 10:29:53 +0200

    Implement add action

---
+++ README.md
@@ -0,0 +1,7 @@
+## mgast
+
+### Install
+
+```sh
+cargo install --git https://github.com/nijo-a/mgast
+```
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use anyhow::{Context, Result, anyhow};
 use clap::{Parser, ValueEnum};
+use colored::Colorize;
+use inquire::{Editor, Text};
 use serde::Serialize;
 use sqlglot_rust::{Dialect, parse};
-use std::{fs, path::PathBuf};
+use std::{fs, path::PathBuf, println};
 
 #[derive(Clone, Copy, Debug, Serialize, PartialEq, ValueEnum)]
 #[serde(rename_all = "kebab-case")]
-enum VmgAction {
+enum Action {
     Add,
     Check,
 }
@@ -14,7 +16,7 @@ enum VmgAction {
 #[derive(Parser, Debug)]
 #[command(version)]
 struct Args {
-    action: VmgAction,
+    action: Action,
 
     #[arg(help = "Migration directory")]
     directory: PathBuf,
@@ -39,11 +41,80 @@ fn validate_files(directory: &PathBuf) -> Result<()> {
         let query = fs::read_to_string(entry.path())?;
 
         if parse(&query, Dialect::Postgres).is_ok() {
-            println!("{file_name} passed");
+            let output = format!("{} passed", file_name);
+            println!("{}", output.green());
         } else {
-            println!("{file_name} failed");
+            let output = format!("{} failed", file_name);
+            println!("{}", output.red());
         };
     }
+
+    Ok(())
+}
+
+fn add_migration(directory: &PathBuf) -> Result<()> {
+    let files = fs::read_dir(directory)
+        .with_context(|| format!("Failed to read directory '{:?}'", directory))?;
+
+    let mut ver = 1;
+
+    let mut digits = 1;
+
+    for file in files {
+        let entry = file?;
+
+        let file_name = entry.file_name();
+        let file_name = file_name
+            .to_str()
+            .ok_or(anyhow!("Failed to parse file name"))?;
+
+        let parts = file_name.split("_").collect::<Vec<_>>();
+
+        if parts.len() <= 1 {
+            continue;
+        }
+
+        if let Some(first) = parts.get(0) {
+            digits = digits.max(first.len());
+
+            if let Ok(file_ver) = u16::from_str_radix(*first, 10) {
+                ver = ver.max(file_ver);
+            }
+        }
+    }
+
+    let next_ver = (ver + 1).to_string();
+    let next_ver = "0".repeat(digits - next_ver.len()) + &next_ver;
+
+    let query = Editor::new("Input query")
+        .with_file_extension(".sql")
+        .prompt()?;
+
+    if query.is_empty() {
+        return Err(anyhow!("No input given"));
+    }
+
+    if parse(&query, Dialect::Postgres).is_err() {
+        return Err(anyhow!("Invalid query"));
+    }
+
+    let name = Text::new("Migration name")
+        .with_help_message(&format!("Ver {}", next_ver))
+        .prompt()?;
+
+    let file_name = format!("{}_{}.sql", next_ver, name.replace(" ", "_").to_lowercase());
+
+    let mut full_path = directory.clone();
+    full_path.push(&file_name);
+
+    let query_preview = &query[..(query.len() - 1).min(50)];
+
+    println!("Writing query to {full_path:?}...");
+    println!("{query_preview}");
+
+    fs::write(full_path, query)?;
+
+    println!("Success!");
 
     Ok(())
 }
@@ -56,8 +127,8 @@ fn main() -> Result<()> {
     }
 
     match args.action {
-        VmgAction::Add => {}
-        VmgAction::Check => validate_files(&args.directory)?,
+        Action::Add => add_migration(&args.directory)?,
+        Action::Check => validate_files(&args.directory)?,
     };
 
     Ok(())
